eeb00ad2f3179687e67d0d920ecc993c5efb58cd
Kunal SAgar <kunalsagar1999@gmail.com>
2020-10-05 13:59:57 +0530

    init

---
+++ Source/candyCrush/candyCrushGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "candyCrushGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CANDYCRUSH_API AcandyCrushGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
